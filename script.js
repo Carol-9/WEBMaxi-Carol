431176a9df7e22612a051ed6444e1bba23ca747a
Carol-9 <carolinebaptista59@gmail.com>
2025-05-19 20:54:27 -0300

    bacon

---
+++ script.js
@@ -0,0 +1,9 @@
+const imagem = document.getElementById('imagem-principal');
+
+imagem.addEventListener('mouseover', () => {
+    imagem.src = 'ybxlO.jpg'; 
+});
+
+imagem.addEventListener('mouseout', () => {
+    imagem.src = 'b983w.jpg'; 
+});
